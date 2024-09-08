8f7999c3d6793f8e9f8290f285b57f8908d5167c
amirrezash0n <159563194+amirrezash0n@users.noreply.github.com>
2024-09-08 21:50:07 +0330

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,14 @@
 const navToggleIcon = document.querySelector('.nav-toggle');
 const menu = document.querySelector('.nav-list');
 const portfolioList = document.querySelectorAll('.portfolio-list__item')
+const scrollHeader = document.querySelector('.header__scroll')
+const buttonToUp = document.querySelector('.footer-top__btn')
+const statisticsContentPercents = document.querySelectorAll(".statistics-content__percent")
+const btnCounter = document.querySelector("#btn__counter")
 
+
+
+let count = 0
 navToggleIcon.addEventListener('click', function() {
     this.classList.toggle('nav-toggle--open');
     menu.classList.toggle('nav-list--open');
@@ -17,3 +24,47 @@ portfolioList.forEach(portfolioList => {
         document.querySelector(contentId).classList.add('portfolio__wrapper--show')
     })
 })
+
+// For Header Scroll Y ==> Top Header
+window.addEventListener('scroll' , function () {
+    let scrollTop = window.scrollY
+    let documentHeight = document.body.clientHeight
+    let windowInnerHeight = window.innerHeight
+    let scrollPercent = (scrollTop / (documentHeight - windowInnerHeight) * 100)
+    let scrollPercentRounded = Math.round(scrollPercent)
+    scrollHeader.style.width = scrollPercentRounded + '%'
+})
+
+buttonToUp.addEventListener('click' , function () {
+    window.scrollTo(0 , 0)
+})
+
+window.addEventListener("scroll" , () => {
+    statisticsContentPercents.forEach(statisticsContentPercent => {
+        if (btnCounter.offsetTop < Math.floor(window.scrollY)){
+            setInterval(() => {
+                const target = parseInt(statisticsContentPercent.dataset.count)
+                if (count < target) {
+                    count++
+                    statisticsContentPercent.innerHTML = count + "%"
+                } else {
+                    statisticsContentPercent.innerHTML = target + "%"
+                }
+            },200)
+        }
+    })
+})
+
+
+const homeLeftTitle = document.querySelector("#home-left__title")
+
+let homeLeftTitleTypeWriter = new Typewriter(homeLeftTitle, {
+  loop: true,
+});
+
+homeLeftTitleTypeWriter 
+    .typeString("Wilkerson")
+    .pauseFor(1000)
+    .typeString(" Hardin")
+    .pauseFor(2500)
+    .start()
